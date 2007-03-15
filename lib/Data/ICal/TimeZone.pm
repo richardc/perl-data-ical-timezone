7f3b5f45d697f6f23b5e05d0d342ba13564cdcd6
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2007-03-15 12:35:39 +0000

    initial sketch
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Data-ICal-TimeZone@4376 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Data/ICal/TimeZone.pm
+++ lib/Data/ICal/TimeZone.pm
@@ -1,12 +1,70 @@
 package Data::ICal::TimeZone;
 use strict;
+use Carp;
+use UNIVERSAL::require;
 
 sub new {
     my $class = shift;
     my %args  = @_;
-    return bless {}, $class;
+    my $timezone = delete $args{timezone}
+      or croak "No timezone specified";
+    my $tz = __PACKAGE__."::$timezone";
+    $tz =~ s{/}{::}g;
+    $tz->require or croak "Couldn't require $tz: $@";
+    return $tz->new;
+}
+
+package Data::ICal::TimeZone::_Base;
+use base qw( Class::Singleton Class::Accessor );
+__PACKAGE__->mk_accessors(qw( _cal ));
+use Data::ICal;
+
+sub new {
+    my $self = shift;
+    return $self->instance;
+}
+
+sub event {
+    my $self = shift;
+    my @zones = grep {
+        $_->ical_entry_type eq 'VTIMEZONE'
+    } @{ $self->_cal->entries };
+    return $zones[0];
+}
+
+sub property {
+    my $self = shift;
+    my $time = shift;
+    return [ $time => { TZID => $self->timezone } ];
+}
+
+sub _load {
+    my $self = shift;
+    my $ics = shift;
+    my $cal = Data::ICal->new( data => $ics );
+    $self->_cal( $cal );
+    return;
 }
 
 1;
 __END__
 
+=head1 NAME
+
+  Data::ICal::TimeZone - timezones for Data::ICal
+
+=head1 SYNOPSIS
+
+  use Data::ICal;
+  use Data::ICal::TimeZone;
+
+  my $cal = Data::ICal->new;
+  my $zone = Data::ICal::TimeZone->new( timezone => 'Europe/London' );
+  $cal->add_event( $zone->event );
+  my $meeting = Data::ICal::Event->new;
+  $meeting->add_properties(
+    summary => 'Go to the pub',
+    dtstart => [ $opening_time, { TZID => $zone->timezone } ],
+    dtend   => $zone->property( $closing_time ),
+  );
+  $cal->add_event( $meeting );
